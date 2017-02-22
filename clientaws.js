682f09e1faa481b6178d88ee05adc9c0b4070d62
vishaljain123 <vishal790@outlook.com>
2017-02-22 14:15:56 +0530

    commit

--- clientaws.js
+++ clientaws.js
@@ -1,6 +1,19 @@
+ 
+
+
  function socketObj(){
 
       	var socket = io('http://ec2-52-33-224-171.us-west-2.compute.amazonaws.com:3000');
-      	return socket;
+      	return "socket";
       }
 
+// function usethisone(){
+
+// 	alert("hey iuser");
+// }
+
+
+// function todaydate(){
+    
+//     alert("heyyyyyy");
+// }
